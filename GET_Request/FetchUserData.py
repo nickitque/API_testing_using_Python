98dfd9494c49015188ab0d7e174e986058810a1d
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-02-21 16:20:13 +0300

    Added docstrings and asserts

--- GET_Request/FetchUserData.py
+++ GET_Request/FetchUserData.py
@@ -1,9 +1,10 @@
+"""In this file I'm Fetching Data from the Json content."""
+
 import requests
 import json
 import jsonpath
 
 """API URLS"""
-
 url = "https://reqres.in/api/users?page=2"
 
 """Send Get Request """
@@ -11,12 +12,17 @@ response = requests.get(url)
 print(response)
 
 """Display Response Content"""
-#print(response.content)
-#print(response.headers)
+# print(response.content)
+# print(response.headers)
 
 """Parse response to Json format"""
 json_response = json.loads(response.text)
 print(json_response)
 
 """Fetch value using Json path"""
-pages = json_path = jsonpath.jsonpath(json_response, 'total_pages')
+pages = jsonpath.jsonpath(json_response, 'total_pages')
+print(f"Total quantity of pages is {pages}")
+
+"""Added asserts: positive and negative."""
+assert pages == [2]
+assert pages != [3]
